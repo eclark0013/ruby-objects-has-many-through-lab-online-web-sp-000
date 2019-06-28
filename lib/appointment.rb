9baf73c95949b451ec4a4443d462285d996fd249
Eric Clark <eclark0013@gmail.com>
2019-06-28 19:21:56 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,21 @@
+class Appointment
+
+  attr_accessor :name, :doctor, :patient
+
+  @@all = []
+
+  def initialize(name, doctor, patient)
+    @name = name
+    @doctor = doctor
+    @patient = patient
+    @@all<<self
+    @doctor.appointments<<self
+    @patient.appointments<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+end
