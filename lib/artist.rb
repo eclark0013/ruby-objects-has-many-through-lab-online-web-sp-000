5b32b781c6cafb3170708401771e7d31c4e6230d
Eric Clark <eclark0013@gmail.com>
2019-06-28 19:00:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,21 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
     @@all<<self
   end
 
   def self.all
     @@all
   end
+
+  def new_song(name, genre)
+    new_song = Song.new(self, name, genre)
+
 
 end
