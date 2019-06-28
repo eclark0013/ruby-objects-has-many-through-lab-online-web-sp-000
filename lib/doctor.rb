879d8fe658cd1a12a3fc6d7a2f0736c42a2509ec
Eric Clark <eclark0013@gmail.com>
2019-06-28 19:25:51 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,8 +14,8 @@ class Doctor
     @@all
   end
 
-  def new_appointment(name, patient)
-    Appointment.new(name, self, patient)
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
   end
 
   def appointments
